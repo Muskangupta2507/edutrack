c0ab699e4adfed9d445152e80f46c685d83ba58d
Muskangupta2507 <100120446+Muskangupta2507@users.noreply.github.com>
2024-04-13 20:19:04 +0530

    piechart initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar';
+import Piechart from './components/Picharts/Piechart';
 function App() {
   return (
     <div>
       <Navbar/>
+      <Piechart/>
     </div>
   )
 }
