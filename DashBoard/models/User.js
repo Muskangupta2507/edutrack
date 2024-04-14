bdf64c7fcb57d107b1add9def88490c7ae162cf3
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-04-14 20:37:28 +0530

    Dashboard setup

---
+++ DashBoard/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+    {
+        name:{
+            type :String,
+            required : true,
+            min: 2,
+            max: 100
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:trusted
+        },
+        enrollmentDate: {
+            type: Date,
+            default: Date.now // Optional: set a default value to the current date
+        },
+        city:String,
+        state:String,
+        country:String,
+        course:String,
+        contactNo:Number,
+        courseProgress:{
+            type: Number,
+            min:0,
+            max:100
+        }
+    }
+);
+
+const User = mongoose.model("User",UserSchema);
+export default User;
