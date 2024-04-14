3c6b005e8f71d11ee293cc2f2037149cfa79aa40
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-04-14 19:49:59 +0530

    Dashboard setup

--- dashboard-frontend/src/Components/Navbar.jsx
+++ dashboard-frontend/src/Components/Navbar.jsx
@@ -34,6 +34,18 @@ const Navbar = () => {
             </IconButton>
           </FlexBetween>
         </FlexBetween>
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={()=>dispatch(setMode())}>
+            {theme.palette.mode === 'dark'?(
+              <DarkModeOutlined sx={{fontSize:"25px"}}/>
+            ):(
+              <LightModeOutlined sx={{fontSize:"25px"}}/>
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined/>
+          </IconButton>
+        </FlexBetween>
       </Toolbar>
     </AppBar>
   )
