766873075a637d927a63486cf825a13a3f6a21e4
Muskangupta2507 <100120446+Muskangupta2507@users.noreply.github.com>
2024-04-16 18:16:00 +0530

    pushagain

--- dashboard-frontend/src/Components/Sidebar.jsx
+++ dashboard-frontend/src/Components/Sidebar.jsx
@@ -5,14 +5,13 @@ import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
-import Profileimg from "../assets/image.png";
 
 const navItems=[
     {
         text:"Dashboard",
         icon: <HomeOutlined />
     },{
-        text:"Students",
+        text:"Information",
         icon: null
     },{
         text:"Courses",
@@ -145,38 +144,6 @@ const Sidebar=({user,drawerWidth,isSidebarOpen,setIsSidebarOpen,isNonMobile}) =>
 
           <Box position="absolute" bottom="2rem">
             <Divider />
-            {/* <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
-              <Box
-                component="img"
-                alt="profile"
-                src={Profileimg}
-                height="40px"
-                width="40px"
-                borderRadius="50%"
-                sx={{ objectFit: "cover" }} 
-              />
-              <Box textAlign="left">
-                <Typography
-                  fontWeight="bold"
-                  fontSize="0.9rem"
-                  sx={{ color: theme.palette.secondary[100] }}
-                >
-                  {user.name}
-                </Typography>
-                <Typography
-                  fontSize="0.8rem"
-                  sx={{ color: theme.palette.secondary[200] }}
-                >
-                  {user.occupation}
-                </Typography>
-              </Box>
-              <SettingsOutlined
-                sx={{
-                  color: theme.palette.secondary[300],
-                  fontSize: "25px ",
-                }}
-              /> 
-            </FlexBetween>*/}
           </Box>
         </Drawer>
       )}
