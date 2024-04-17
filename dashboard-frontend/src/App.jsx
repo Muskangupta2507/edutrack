6be0b27b7a55581ed462129f5f8880506b3f4185
Muskangupta2507 <100120446+Muskangupta2507@users.noreply.github.com>
2024-04-17 12:30:04 +0530

    nivo

--- dashboard-frontend/src/App.jsx
+++ dashboard-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import {createTheme} from '@mui/material/styles'
 import {themeSettings} from './theme';
 import { useSelector } from 'react-redux';
-import {BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {Navigate, Route, Routes } from 'react-router-dom';
 import DashBoard from './Pages/DashBoard';
 import Layout from './Pages/Layout';
 import Courses from "./Pages/Courses.jsx"
@@ -26,13 +26,6 @@ const App = () => {
               <Route path="/courses" element={<Courses />} />
               <Route path="/students" element={<Student />} />
               <Route path="/overview" element={<Overview />} />
-              {/* <Route path="/transactions" element={<Transactions />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/daily" element={<Daily />} />
-              <Route path="/monthly" element={<Monthly />} />
-              <Route path="/breakdown" element={<Breakdown />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/performance" element={<Performance />} /> */}
             </Route>
           </Routes>
         </ThemeProvider>
